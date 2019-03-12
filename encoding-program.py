c79f7f6368fa1cd1649c23e03030e0fc5d35dbff
EktaSmothra <ektasamotra@gmail.com>
2019-03-12 11:09:08 +0530

    Add files via upload

---
+++ encoding-program.py
@@ -0,0 +1,24 @@
+import chardet
+import urllib.request
+import codecs
+
+file = input("enter name of the file: ")
+with open(file, 'rb') as f:
+    data = f.read()
+    det = chardet.detect(data)['encoding']
+    print(det)
+    # det = chardet.detect(file)['encoding']
+#   encoding=chardet.detect(data).get("encoding")
+#   print(encoding)
+
+
+rawdata = urllib.request.urlopen('http://yahoo.co.jp/').read()
+print(chardet.detect(rawdata))
+
+print("Conveting it into utf-8 encoding...")
+target = input("give a name to your target file: ")
+with codecs.open(file, "r") as sourceFile:
+    with codecs.open(target, "w", "utf-8") as targetFile:
+        contents = sourceFile.read()
+        result = targetFile.write(contents)
+        print(result)
