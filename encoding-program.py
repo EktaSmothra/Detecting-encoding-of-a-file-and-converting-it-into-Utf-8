6f86fff897871d640a20ce6237e1723124fddfc9
EktaSmothra <ektasamotra@gmail.com>
2019-03-14 23:28:33 +0530

    Update encoding-program.py

--- encoding-program.py
+++ encoding-program.py
@@ -2,14 +2,18 @@ import chardet
 import urllib.request
 import codecs
 
-file = input("enter name of the file: ")
-with open(file, 'rb') as f:
-    data = f.read()
-    det = chardet.detect(data)['encoding']
+file = input("enter name of the file: ")            # for getting the file name from the user
+with open(file, 'rb') as f:                         # opening the file entered by user in read-binary mode so that strings can be encoded
+    data = f.read()                                 # reading the contents of th file
+    det = chardet.detect(data)['encoding']          # detecting encoding of the file
     print(det)
 
-rawdata = urllib.request.urlopen('http://yahoo.co.jp/').read()
+# library used to read contents of a web page in web browser. This is a way for reading web page content and detecting encoding in
+# web browser
+rawdata = urllib.request.urlopen('http://yahoo.co.jp/').read()      
 print(chardet.detect(rawdata))
+
+# Converting file to utf encoding 
 
 print("Conveting it into utf-8 encoding...")
 target = input("give a name to your target file: ")
